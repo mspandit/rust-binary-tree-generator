9991a499ff67051cad8946ca162a935f31050453
Milind S. Pandit <mspandit@yahoo.com>
2026-04-28 09:59:21 -0700

    Successful implementation of stack as closure.

--- src/generator_state.rs
+++ src/generator_state.rs
@@ -1,62 +1,70 @@
+use std::fmt::Debug;
 use crate::{Token, binary_tree::BinaryTree, grammar::Grammar, stack::Stack};
 
 #[derive(Debug)]
-pub struct GeneratorState<T: Token>(Vec<Stack<T>>);
+pub struct GeneratorState<T: Token + Debug>(Vec<Stack<BinaryTree<T>>>);
 
-impl<T: Token> Default for GeneratorState<T> {
+impl<T: Token + Debug> Default for GeneratorState<T> {
     fn default() -> Self {
         Self(vec![Stack::default()])
     }
 }
 
-fn process_fn<T: Token>(token: T, grammar: &Grammar<T>)
--> impl Fn(Vec<Stack<T>>, Stack<T>) -> Vec<Stack<T>> {
-    move |new_stacks, current_stack| {
-        grammar.lookup_terminals(& token).map_or(
-            new_stacks.clone(),
-            |t_labels| t_labels.iter().fold(
-                new_stacks,
-                |mut new_stacks, terminal_label| {
-                    new_stacks.append(&mut current_stack.clone()
-                        .shift_reduce(
-                            BinaryTree::Terminal {
-                                label: terminal_label.clone(),
-                                token: token.clone()
-                            },
-                            grammar
-                        )
-                    );
-                    new_stacks
-                }
-            )
-        )
-    }
-}
-
-impl<T: Token> GeneratorState<T> {
+impl<T: Token + Debug + 'static> GeneratorState<T> {
     #[cfg(test)]
     pub fn len(self: & Self) -> usize {
         self.0.len()
     }
 
     pub fn process(self: Self, token: T, grammar: &Grammar<T>) -> Self {
-        let stack_fn = process_fn(token, grammar);
         Self(
             self.0.into_iter().fold(
                 Vec::default(),
-                stack_fn
+                |new_stacks, current_stack| {
+                    grammar.lookup_terminals(& token).map_or(
+                        new_stacks.clone(),
+                        |t_labels| {
+                            t_labels.iter().fold(
+                            new_stacks,
+                            |mut new_stacks, terminal_label| {
+                                new_stacks.append(&mut current_stack.clone()
+                                    .shift_reduce(
+                                        BinaryTree::Terminal {
+                                            label: terminal_label.clone(),
+                                            token: token.clone()
+                                        },
+                                        grammar
+                                    )
+                                );
+                                new_stacks
+                            }
+                        )}
+                    )
+                }
             )
         )
     }
 
     pub fn tops(self: Self) -> Vec<BinaryTree<T>> {
-        self.0.into_iter().map(|stack| stack.top()).collect()
+        self
+            .0
+            .into_iter()
+            .map(|stack| match stack.0 {
+                 None => panic!("Cannot get top of empty stack"),
+                 Some(ref f) => f().0,
+            }).collect()
     }
 
     pub fn filter_stacks(self: Self) -> Self {
         Self(
             self.0.into_iter()
-                .filter(|stack| 1 == stack.len())
+                .filter(|stack| match stack.0 {
+                     None => false,
+                     Some(ref f) => match f() {
+                         (_, Stack(None)) => true,
+                         (_, Stack(Some(_))) => false
+                     }
+                })
                 .collect()
         )
     }
--- src/stack.rs
+++ src/stack.rs
@@ -1,73 +1,76 @@
-use std::fmt::Display;
+use std::{fmt::{Debug, Display}, rc::Rc};
 
 use crate::{Token, binary_tree::BinaryTree, grammar::Grammar};
 
-#[derive(Debug, Clone, Default)]
-pub struct Stack<T: Token>(Vec<BinaryTree<T>>);
+#[derive(Clone)]
+pub struct Stack<S: Debug + Clone>(pub Option<Rc<dyn Fn() -> (S, Self)>>);
 
-impl<T: Token> Stack<T> {
+impl<S: Clone + Debug> Default for Stack<S> {
+    fn default() -> Self {
+        Self(None)
+    }
+}
+
+impl<S: Debug + Clone> Debug for Stack<S> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.0 {
+            None => write!(f, "Empty"),
+            Some(_) => write!(f, "Element()")
+        }
+    }
+}
+
+impl<T: Token + Debug + 'static> Stack<BinaryTree<T>> {
     pub fn top(&self) -> BinaryTree<T> {
-        self.0[0].clone()
+        match self.0 {
+             None => panic!("Cannot get top of empty stack"),
+             Some(ref f) => f().0,
+        }
     }
 
-    pub fn len(&self) -> usize {
-        self.0.len()
+    fn pop(self: Self) -> (BinaryTree<T>, Self) {
+        match self.0 {
+            None => panic!("Cannot pop from empty stack"),
+            Some(ref f) => f(),
+        }
     }
 
-    fn pop(&mut self) -> Option<BinaryTree<T>> {
-        self.0.pop()
+    fn push(self: Self, element: BinaryTree<T>) -> Self {
+        let closure = move || (element.clone(), self.clone());
+        Stack(Some(Rc::new(closure)))
     }
 
-    fn push(&mut self, tree: BinaryTree<T>) {
-        self.0.push(tree);
-    }
-
-    pub fn shift_reduce(
-        mut self: Self,
-        tree: BinaryTree<T>,
-        grammar: & Grammar<T>
-    ) -> Vec<Self> {
-        match self.pop() {
-            None => {
-                self.push(tree); // shift only
-                vec![self]
-            },
-            Some(popped_tree) => {
-                let r = self.clone(); // for recursion later
-                // restore the popped tree
-                self.push(popped_tree.clone());
-                self.push(tree.clone()); // shift
-                let new_stacks = vec![self.clone()];
-
-                match grammar.lookup_nonterminals(
-                    &(popped_tree.label(), tree.label())
-                ) {
-                    None => new_stacks,
-                    Some(new_nonterminal_labels) => {
-                        new_nonterminal_labels.iter().fold(
-                            new_stacks,
-                            |mut new_stacks, new_nonterminal_label| {
-                                let new_nonterminal = BinaryTree::Nonterminal {
-                                    label: new_nonterminal_label.clone(),
-                                    left: Box::new(popped_tree.clone()),
-                                    right: Box::new(tree.clone()),
-                                };
-                                new_stacks.append(&mut r.clone()
-                                    .shift_reduce(new_nonterminal, grammar)
-                                );
-                                new_stacks
-                            }
-                        )
+    pub fn shift_reduce(self: Self, mut tree: BinaryTree<T>, grammar: &Grammar<T>) -> Vec<Self> {
+        let mut stack = self.clone();
+        let mut retval = vec![stack.clone().push(tree.clone())];
+        loop {
+            match stack.0 {
+                None => break,
+                Some(ref f) => {
+                    let (popped, rest) = f();
+                    match grammar.lookup_nonterminals(&(popped.label(), tree.label())) {
+                        None => { break },
+                        Some(new_nonterminal_label) => {
+                            let new_nonterminal = BinaryTree::Nonterminal {
+                                label: new_nonterminal_label.clone(),
+                                left: Box::new(popped.clone()),
+                                right: Box::new(tree.clone())
+                            };
+                            retval.push(rest.clone().push(new_nonterminal.clone()));
+                            tree = new_nonterminal;
+                        }
                     }
+                    stack = rest;
                 }
             }
         }
+        retval
     }
 }
 
 impl Display for Stack<char> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self.0)
+        write!(f, "{:?}", self)
     }
 }
 
@@ -77,10 +80,20 @@ mod test {
 
     #[test]
     fn test_reduce_second_character() {
-        let stack = Stack(vec![BinaryTree::Terminal{ label: "UnOp".to_string(), token: '-' }]);
+        let stack = Stack(Some(
+            Rc::new(
+                || (
+                    BinaryTree::Terminal {
+                        label: "UnOp".to_string(),
+                        token: '-',
+                    },
+                    Stack(None)
+                )
+            )
+        ));
         let x = stack.shift_reduce(BinaryTree::Terminal{ label: "E".to_string(), token: '1' }, &Grammar::expression());
         println!("{x:?}");
-        match x[1].0[0] {
+        match x[1].top() {
             BinaryTree::Terminal { label: _, token: _, } => panic!("Expected a nonterminal, got {x:?}"),
             BinaryTree::Nonterminal { label: _, left: _, right: _ } => (),
         }
@@ -88,11 +101,26 @@ mod test {
 
     #[test]
     fn test_reduce_third_character() {
-        let stack = Stack(vec![
-            BinaryTree::Terminal{ label: "E".to_string(), token: 'b' },
-            BinaryTree::Terminal{ label: "E".to_string(), token: 'a' },
-        ]);
+        let stack = Stack(Some(
+            Rc::new(|| (
+                BinaryTree::Terminal{
+                    label: "E".to_string(),
+                    token: 'b'
+                },
+                Stack(Some(
+                    Rc::new(
+                        || (
+                            BinaryTree::Terminal{
+                                label: "E".to_string(),
+                                token: 'a'
+                            },
+                            Stack(None)
+                        )
+                    )
+                ))
+            ))
+        ));
         let x = stack.shift_reduce( BinaryTree::Terminal{ label: "E".to_string(), token: 'c' }, &Grammar::expression());
-        assert_eq!(3, x[0].len(), "{x:?}");
+        assert_eq!(1, x.len(), "{x:?}");
     }
 }
